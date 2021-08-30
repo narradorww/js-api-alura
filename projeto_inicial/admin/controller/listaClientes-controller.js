242ed1e48bfc439bf962f4580ea76984b5489906
narradorww <rodrigo.anst@gmail.com>
2021-08-29 22:04:13 -0300

    Criada a feature de deletar clientes e editar clientes

--- projeto_inicial/admin/controller/listaClientes-controller.js
+++ projeto_inicial/admin/controller/listaClientes-controller.js
@@ -1,38 +1,49 @@
-import { clienteService } from '../service/client-service.js'
-
-
+import { clienteService } from "../service/client-service.js";
 
 /*Criando um template par receber os dados da API */
 /*Criamos  uma constante para criar cada linha no nosso site,
  atribuindo a ela o valor daddo por uma função anonima
- cujo os argumentos são os dados que queremos - nome e email.
+ cujo os argumentos são os dados que queremos - nome e email, id.
  */
 
- const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
+const criaNovaLinha = (nome, email, id) => {
+  const linhaNovoCliente = document.createElement("tr");
+  const conteudo = `
         <td class="td" data-td>${nome}</td>
                     <td>${email}</td>
                     <td>
                         <ul class="tabela__botoes-controle">
-                            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                            <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
                             <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
                         </ul>
                     </td> 
-                    `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
-
+                    `;
+  linhaNovoCliente.innerHTML = conteudo;
+  linhaNovoCliente.dataset.id = id;
+  return linhaNovoCliente;
+};
 
 /* seleciona no html o local onde serão inseridos os dados, atraés de QuerySelector, adiciona ao child de 'data-tabela' ('tr')a linha criada
 iterando na array da API com forEach e atribuindo os valores aos elementos, nesse caso nome e email*/
 
+const tabela = document.querySelector("[data-tabela]");
 
-const tabela = document.querySelector('[data-tabela]')
+tabela.addEventListener("click", (evento) => {
+  let ehBotaoDeletar =
+    evento.target.className === "botao-simples botao-simples--excluir";
+  if (ehBotaoDeletar) {
+    const linhaCliente = evento.target.closest("[data-id]");
+    let id = linhaCliente.dataset.id;
+    clienteService.removeCliente(id).then(() => {
+      linhaCliente.remove();
+    });
+  }
+});
 
-clienteService.listaClientes()
-.then(data => {
-        data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email))
-})})
+clienteService.listaClientes().then((data) => {
+  data.forEach((elemento) => {
+    tabela.appendChild(
+      criaNovaLinha(elemento.nome, elemento.email, elemento.id)
+    );
+  });
+});
--- projeto_inicial/admin/service/client-service.js
+++ projeto_inicial/admin/service/client-service.js
@@ -1,31 +1,65 @@
-
-const listaClientes =() => {
-        return fetch(`http://localhost:3000/profile`)
-        .then( resposta => {
-            return resposta.json();
-        })}
-          
+const listaClientes = () => {
+  return fetch(`http://localhost:3000/profile`).then((resposta) => {
+    return resposta.json();
+  });
+};
 
 // criando um  cliente
 
-const criaCliente =( nome, email ) => {
-        return fetch(`http://localhost:3000/profile`,{
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({
+const criaCliente = (nome, email) => {
+  return fetch(`http://localhost:3000/profile`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      nome: nome,
+      email: email,
+    }),
+  }).then((resposta) => {
+    return resposta.body;
+  });
+};
+
+//função de remover cliente 
+
+const removeCliente = (id) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: "DELETE",
+  });
+};
+
+
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+        .then(resposta => {
+            return resposta.json()
+        })
+}
+
+
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`,{
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
             nome: nome,
             email: email
         })
     })
-    .then ( resposta => {
-        return resposta.body
-        }
-    )
-    }    
+        .then(resposta => {
+            return resposta.json()
+        })
+}
+
+
 //exporta para todo o sistema o resultado da promise através de um objeto
 
-    export const clienteService = { 
-        listaClientes,
-        criaCliente, 
-        
-    }
+export const clienteService = {
+  listaClientes,
+  criaCliente,
+  removeCliente,
+  detalhaCliente,
+  atualizaCliente
+};
